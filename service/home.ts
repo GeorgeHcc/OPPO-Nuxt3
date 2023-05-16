6b8b8afba1b8e60ae1f24ef4d1ca59e8910650e2
GeorgeH <default_hyn@163.com>
2023-05-16 18:43:31 +0800

    update service

--- service/home.ts
+++ service/home.ts
@@ -1,4 +1,4 @@
-import hyRequest from "./index";
+import ApiRequest from "./index";
 import cache from "~/utils/Cache";
 import { useHomeStore } from "@/store/home";
 
@@ -11,13 +11,8 @@ interface IHomeInfo {
 }
 
 export const getHomeInfo = () => {
-    if(process.client){
-        if(cache.getItem("banners")&&cache.getItem("categorys")&&cache.getItem("navbars")){
-            
-        }
-    }
   // 这个请求只能在 setup 或 lifecycle 中请求
-  return hyRequest.get<ResultType<IHomeInfo>>(
+  return ApiRequest.get<ResultType<IHomeInfo>>(
     "/home/info",
     { type: "oppo" },
     {
@@ -27,7 +22,6 @@ export const getHomeInfo = () => {
         homeStore.changeNavBar(response._data.data.navbars);
         homeStore.changeBanners(response._data.data.banners);
         homeStore.changeCategorys(response._data.data.categorys);
-        
       },
     }
   );
@@ -35,12 +29,12 @@ export const getHomeInfo = () => {
 
 export const getHomeOnePlusInfo = () => {
   // 这个请求只能在 setup 或 lifecycle 中请求
-  return hyRequest.get<ResultType<IHomeInfo>>("/home/info", {
+  return ApiRequest.get<ResultType<IHomeInfo>>("/home/info", {
     type: "onePlus",
   });
 };
 export const getHomeIntelligentInfo = () => {
-  return hyRequest.get<ResultType<IHomeInfo>>("/home/info", {
+  return ApiRequest.get<ResultType<IHomeInfo>>("/home/info", {
     type: "intelligent",
   });
 };
--- store/home.ts
+++ store/home.ts
@@ -7,6 +7,23 @@ export const useHomeStore = defineStore("home", {
     banners: [],
     categorys: [],
   }),
+  getters:{
+    getNavBar(){
+      if(this.navbars.length<1){
+        return JSON.parse(Cache.getItem("navbars"))
+      }
+    },
+    getBanners(){
+      if(this.banners.length<1){
+        return JSON.parse(Cache.getItem("banners"))
+      }
+    },
+    getCategorys(){
+      if(this.categorys.length<1){
+        return JSON.parse(Cache.getItem("categorys"))
+      }
+    }
+  },
   actions: {
     changeNavBar(payload: INavBar[]) {
       this.navbars = payload;
